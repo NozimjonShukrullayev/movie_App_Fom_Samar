f46e3e8d4ed731d5596b6405df92fd04b2bffeaf
NozimjonShukrullayev <nozimshukrullayev@gmail.com>
2024-04-19 09:38:15 +0500

    second branch changed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import AppInfo from '../app-info/App-info';
 import MoviesAddForm from '../movies-add-form/Movies-add-form';
 import MoviesList from '../movies-list/Movies-list';
 import SearchPanel from '../search-panel/Search-panel';
+import TextComponent from '../text-component/TextComponent';
 import './App.css';
 import { useContext } from 'react';
 import { Context } from '../../Context/Context';
@@ -41,6 +42,7 @@ function App() {
         {isLoading && "Loading..."}
         <MoviesList />
         <MoviesAddForm />
+        <TextComponent />
       </div>
     </div>
   );
